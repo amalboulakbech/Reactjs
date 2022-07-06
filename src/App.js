dd35b2a15b14b78c57ac64d2b18cf09192bc56e8
unknown <boulakbechamal@gmail.com>
2022-07-06 10:15:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Form } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Démystifiez les notion-clés du JS</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Gagnez en confiance en seulement 7 jours grâce à ce cours décryptant
+          l'exécution de votre code JavaScript.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <ul className="list">
+        <li>Réduisez le syndrome de l'imposteur </li>
+        <li>Créer des compétences -quiz a la fin de chaque journée </li>
+        <li>Aprenez rapidement - 10 min de vedios par jour </li>
+      </ul>
     </div>
   );
 }
